e38acddba386be71b23ab0781a13fa7866abded4
JyothsnaObanampalli <78947299+JyothsnaObanampalli@users.noreply.github.com>
2021-04-10 16:10:48 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+            [Route("api/User/Register")]
+            [HttpPost]
+            public IdentityResult Register(Accountmodel model)
+            {
+                var userStore = new UserStore<ApplicationUser>(new ApplicationdbContext());
+                var manager = new UserManager<ApplicationUser>(userStore);
+                var user = new ApplicationUser() { FirstName = model.FirstName, Email = model.Email };
+                user.LastName = model.LastName;
+                manager.PasswordValidator = new PasswordValidator
+                {
+                    RequiredLength = 3
+                };
+                IdentityResult result = manager.Create(user, model.Password);
+                return result;
+            }
+        
+    }
+}
